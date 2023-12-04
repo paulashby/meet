7916d210cb9b6864d9942a037b5747d11603c6c3
Paul Ashby <paul@primitive.co>
2023-12-04 11:29:35 +0000

    Set default timezone to Europe/London and last meeting to end at 5pm

--- config.ts
+++ config.ts
@@ -6,7 +6,7 @@ export const DEFAULT_DURATION = 30
 
 export const CALENDARS_TO_CHECK = ["primary"]
 export const SLOT_PADDING = 0
-export const OWNER_TIMEZONE = "America/Los_Angeles"
+export const OWNER_TIMEZONE = "Europe/London"
 export const LEAD_TIME = 0
 
 const DEFAULT_WORKDAY = [
@@ -15,7 +15,7 @@ const DEFAULT_WORKDAY = [
       hour: 9,
     },
     end: {
-      hour: 22,
+      hour: 17,
     },
   },
 ]
