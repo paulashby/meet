d9dbf812e48edd5cc8d2eb95addab7ca8592e943
Paul Ashby <paul@primitive.co>
2024-03-16 13:10:59 +0000

    Increase intro text size

--- components/Template.tsx
+++ components/Template.tsx
@@ -4,7 +4,7 @@ export default function Template() {
       <h1 className="text-3xl font-bold tracking-tight text-white sm:text-5xl">
         Let&apos;s talk!
       </h1>
-      <p className="mt-2 sm:mt-6 sm:text-xl text-white font-medium">
+      <p className="mt-2 sm:mt-6 text-lg leading-[1.4rem] text-white font-medium">
         My latest availability is below. Choose a date and time that works for
         you and we&apos;ll get something on the books.
       </p>
